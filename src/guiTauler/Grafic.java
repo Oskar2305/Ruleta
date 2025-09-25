3e6dfb120cfb7af76bd8362ea919519d885b7563
alexpotlog <alexpotloghary@gmail.com>
2025-09-25 18:33:28 +0200

    Legibilidad

--- src/guiTauler/Grafic.java
+++ src/guiTauler/Grafic.java
@@ -12,19 +12,15 @@ public class Grafic {
     private boolean apostaAmount;
 
     public void Initialize() throws FontFormatException, IOException {
-        {
-            Font fntDigital = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/CursedTimerULiL.ttf"));
-            fntDigital = fntDigital.deriveFont(Font.PLAIN, 40);
-            Font fntBitty = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/Bittypix.otf"));
-            fntBitty = fntBitty.deriveFont(Font.PLAIN, 40);
-            Font fntResum = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/digital-7 (italic).ttf"));
-            fntResum = fntDigital.deriveFont(Font.PLAIN, 15);
+        Font fntDigital = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/CursedTimerULiL.ttf"));
+        fntDigital = fntDigital.deriveFont(Font.PLAIN, 40);
+        Font fntBitty = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/Bittypix.otf"));
+        fntBitty = fntBitty.deriveFont(Font.PLAIN, 40);
+        Font fntResum = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/digital-7 (italic).ttf"));
+        fntResum = fntDigital.deriveFont(Font.PLAIN, 15);
 
-            frame.setBounds(100, 100, 450, 400);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().setLayout(new BorderLayout(0, 0));
-        }
-
-
+        frame.setBounds(100, 100, 450, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout(0, 0));
     }
 }
