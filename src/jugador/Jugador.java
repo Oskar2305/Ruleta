bc87446e607a337540700af2c7523498d5da6d60
alexpotlog <alexpotloghary@gmail.com>
2025-09-25 17:23:40 +0200

    Creado TODO y creado atributo nombre en Jugador

--- src/jugador/Jugador.java
+++ src/jugador/Jugador.java
@@ -2,8 +2,11 @@ package jugador;
 
 public class Jugador {
     private double money;
+    public final String NOMBRE;
 
-    public Jugador() {
+    public Jugador(String nom) {
+        //TODO: crea forma d'inicialitzar el nom a main
+        NOMBRE = nom;
         this.money = 1000;
     }
 
