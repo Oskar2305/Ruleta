9dd3d1295c0e339a0f399d3909aec4aa48239ece
Alex-Potlog <alexpotloghary@gmail.com>
2025-09-26 13:58:41 +0200

    Crea classe Jugades

---
+++ src/joc/Jugades.java
@@ -0,0 +1,11 @@
+package joc;
+
+public class Jugades {
+    public static boolean jugaNumero(int numEscollit, int result){
+        return false;
+    }
+
+    public static boolean jugaColor(Color colorEscollit, Color colorResult){
+        return false;
+    }
+}
