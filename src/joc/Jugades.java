488931710558350bb81bedaa8e702b21bf025ced
Alex-Potlog <alexpotloghary@gmail.com>
2025-10-01 19:01:16 +0200

    Reformat per complir els meus standarts

--- src/joc/Jugades.java
+++ src/joc/Jugades.java
@@ -10,22 +10,16 @@ public class Jugades {
     }
 
     public static int jugaParell(int numCasella){
-        int parells=0;
-
         if (numCasella!=0){
-            for (int i = 1 ; i <= 36; i++){
-                if (numCasella%2==0){
-                    parells = 2;
-                    return parells;
-                }
-                if (numCasella%2!=0){
-                    parells = 1;
-                    return parells;
-                }
+            if (numCasella % 2 == 0) {
+                return 2;
+            }
+            if (numCasella % 2 != 0) {
+                return 1;
             }
         }
 
-        return parells;
+        return 0;
     }
 
     private Jugades(){}
